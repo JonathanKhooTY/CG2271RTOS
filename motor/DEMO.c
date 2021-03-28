d91ba49d67a2bde56f9d34a31ebc12e491f7dc37
JonathanKhooTY <jonathan.khooty@gmail.com>
2021-03-28 18:20:48 +0800

    Motor driver operation for 2 wheels

--- motor/DEMO.c
+++ motor/DEMO.c
@@ -14,8 +14,10 @@ typedef enum {RED, GREEN, BLUE} colors_t;
 // PWM Motor Constants
 #define PTB0_Pin 0
 #define PTB1_Pin 1
-#define PTE20_Pin 20
-#define PTE21_Pin 21
+#define PTD0_Pin 0
+#define PTD1_Pin 1
+#define PTD2_Pin 2
+#define PTD3_Pin 3
 #define CLOCK_FREQ 48000000
 #define PRESCALER  128
 
@@ -32,6 +34,9 @@ typedef enum {RED, GREEN, BLUE} colors_t;
 #define SW_POS 6
 int on_off = 0;
 
+typedef enum {
+	FORWARD,BACKWARD,LEFT,RIGHT, STOP
+} directions;
 
 void initGPIO() 
 { 
@@ -94,51 +99,52 @@ void PORTD_IRQHandler() {
  * PWM Functions
  *---------------------------------------------------------------------------*/
 void initPWM(uint32_t mod) {
-  SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
-	SIM_SCGC5 |= SIM_SCGC5_PORTE_MASK;
+  SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
 	
-  PORTE->PCR[PTE20_Pin] &= ~PORT_PCR_MUX_MASK; 
-  PORTE->PCR[PTE20_Pin] |= PORT_PCR_MUX(3);
+  PORTD->PCR[PTD0_Pin] &= ~PORT_PCR_MUX_MASK; 
+  PORTD->PCR[PTD0_Pin] |= PORT_PCR_MUX(4);
 	
-	PORTE->PCR[PTE21_Pin] &= ~PORT_PCR_MUX_MASK; 
-  PORTE->PCR[PTE21_Pin] |= PORT_PCR_MUX(3);
+	PORTD->PCR[PTD1_Pin] &= ~PORT_PCR_MUX_MASK; 
+  PORTD->PCR[PTD1_Pin] |= PORT_PCR_MUX(4);
 	
-  PORTB->PCR[PTB0_Pin] &= ~PORT_PCR_MUX_MASK; 
-  PORTB->PCR[PTB0_Pin] |= PORT_PCR_MUX(3);
+	PORTD->PCR[PTD2_Pin] &= ~PORT_PCR_MUX_MASK; 
+  PORTD->PCR[PTD2_Pin] |= PORT_PCR_MUX(4);
+	
+	PORTD->PCR[PTD3_Pin] &= ~PORT_PCR_MUX_MASK; 
+  PORTD->PCR[PTD3_Pin] |= PORT_PCR_MUX(4);
+	
+  //PORTB->PCR[PTB0_Pin] &= ~PORT_PCR_MUX_MASK; 
+  //PORTB->PCR[PTB0_Pin] |= PORT_PCR_MUX(3);
   
-  PORTB->PCR[PTB1_Pin] &= ~PORT_PCR_MUX_MASK;
-  PORTB->PCR[PTB1_Pin] |= PORT_PCR_MUX(3);
+  //PORTB->PCR[PTB1_Pin] &= ~PORT_PCR_MUX_MASK;
+  //PORTB->PCR[PTB1_Pin] |= PORT_PCR_MUX(3);
   
-  SIM_SCGC6 |= SIM_SCGC6_TPM1_MASK;
+  SIM_SCGC6 |= SIM_SCGC6_TPM0_MASK;
+	SIM_SCGC6 |= SIM_SCGC6_TPM0_MASK;
   
   SIM_SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;
   SIM_SOPT2 |= SIM_SOPT2_TPMSRC(1);
   
-  TPM1->MOD = mod;
+  //TPM1->MOD = mod;
+	TPM0->MOD = mod;
   
-  TPM1->SC &= ~(TPM_SC_CMOD_MASK | TPM_SC_PS_MASK);   //Clearing
-  TPM1->SC |= TPM_SC_CMOD(1) | TPM_SC_PS(7); 					//Increments in counter clock, 128 ps
-  TPM1->SC &= ~TPM_SC_CPWMS_MASK;											
+  TPM0->SC &= ~(TPM_SC_CMOD_MASK | TPM_SC_PS_MASK);   //Clearing
+  TPM0->SC |= TPM_SC_CMOD(1) | TPM_SC_PS(7); 					//Increments in counter clock, 128 ps
+  TPM0->SC &= ~TPM_SC_CPWMS_MASK;											
   
-  TPM1_C0SC &= ~(TPM_CnSC_ELSB_MASK | TPM_CnSC_ELSA_MASK | TPM_CnSC_MSB_MASK | TPM_CnSC_MSA_MASK);
-  TPM1_C0SC |= TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1);    //Toggle output on match
+  TPM0_C0SC &= ~(TPM_CnSC_ELSB_MASK | TPM_CnSC_ELSA_MASK | TPM_CnSC_MSB_MASK | TPM_CnSC_MSA_MASK);
+  TPM0_C0SC |= TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1);    //Toggle output on match
   
-  TPM1_C1SC &= ~(TPM_CnSC_ELSB_MASK | TPM_CnSC_ELSA_MASK | TPM_CnSC_MSB_MASK | TPM_CnSC_MSA_MASK);
-  TPM1_C1SC |= TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1);    //Toggle output on match
+  TPM0_C1SC &= ~(TPM_CnSC_ELSB_MASK | TPM_CnSC_ELSA_MASK | TPM_CnSC_MSB_MASK | TPM_CnSC_MSA_MASK);
+  TPM0_C1SC |= TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1);    //Toggle output on match
+	
+	TPM0_C2SC &= ~(TPM_CnSC_ELSB_MASK | TPM_CnSC_ELSA_MASK | TPM_CnSC_MSB_MASK | TPM_CnSC_MSA_MASK);
+  TPM0_C2SC |= TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1);    //Toggle output on match
+	
+	TPM0_C3SC &= ~(TPM_CnSC_ELSB_MASK | TPM_CnSC_ELSA_MASK | TPM_CnSC_MSB_MASK | TPM_CnSC_MSA_MASK);
+  TPM0_C3SC |= TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1);    //Toggle output on match
+
 }  
-
-
-void motor_control(long on_off, uint32_t mod) {
-  
-  if (on_off) {      // On
-    TPM1_C0V = (uint32_t)(mod * DUTY_CYCLE);
-    TPM1_C1V = (uint32_t)(mod * DUTY_CYCLE);
-    
-  } else {          // Off 
-    TPM1_C0V = 0;
-    TPM1_C1V = 0;
-  }
-}
 
 
 // Delay 
@@ -149,6 +155,37 @@ static void delay(volatile uint32_t nof) {
   }
 }
 
+void motor_control(directions direction , uint32_t mod) {
+  
+	switch (direction) {
+		case FORWARD:
+		TPM0_C0V = (uint32_t)(mod * DUTY_CYCLE);
+    TPM0_C2V = (uint32_t)(mod * DUTY_CYCLE);
+		break;
+		case BACKWARD:
+		TPM0_C1V = (uint32_t)(mod * DUTY_CYCLE);
+		TPM0_C3V = (uint32_t)(mod * DUTY_CYCLE);
+		break;
+		case LEFT:
+		TPM0_C1V = (uint32_t)(mod * DUTY_CYCLE);
+    TPM0_C2V = (uint32_t)(mod * DUTY_CYCLE);
+		break;
+		case RIGHT:
+		TPM0_C0V = (uint32_t)(mod * DUTY_CYCLE);
+    TPM0_C3V = (uint32_t)(mod * DUTY_CYCLE);
+		break;
+		case STOP:
+		TPM0_C0V = 0;
+		TPM0_C1V = 0;
+		TPM0_C2V = 0;
+		TPM0_C3V = 0;
+		break;
+	}
+}
+
+
+
+
 
 /*----------------------------------------------------------------------------
  * Main Function
@@ -157,19 +194,41 @@ static void delay(volatile uint32_t nof) {
 int main() {  
 
   
-  SystemCoreClockUpdate();
+SystemCoreClockUpdate();
   initSwitch();  
   initGPIO();
-  
-  uint32_t mod = CLOCK_FREQ / PRESCALER /FREQ;
-
+	uint32_t mod = CLOCK_FREQ / PRESCALER /FREQ;
   initPWM(mod);
-  TPM1_C0V = 0;
-  TPM1_C1V = 0;
-
- 
+	TPM0_C0V = 0;
+  TPM0_C1V = 0;
+	TPM0_C2V = 0;
+  TPM0_C3V = 0;
+	
+	int data = 0; //Data to be received
+	 while (1) {
+		 switch (data) {
+			 case 0:
+			 motor_control(STOP,mod);
+			 break;
+		 case 1:
+			motor_control(FORWARD,mod);
+			break;
+		 case 2:
+			 motor_control(BACKWARD,mod);
+			break;
+		 case 3:
+			 motor_control(LEFT,mod);
+		 break;
+		 case 4:
+			 motor_control(RIGHT,mod);
+		 break;
+		 
+			}
+	 }
+	
   
-  while(1) {	
-		motor_control(on_off, mod);
+
+
+  
+ 
   }
-}
